4e238dc615622da3338bdba8dbd3d5d6055b6e1b
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-03-08 11:04:40 +0330

    add getStaticProps

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,20 +1,25 @@
 import Link from "next/link";
 
-export default function Products() {
+export default function Products({ products }) {
+  console.log(products);
   return (
     <>
       <h1>Products</h1>
       <ul>
-        <li>
-          <Link href={"/products/1"}>Product 1</Link>
-        </li>
-        <li>
-          <Link href={"/products/2"}>Product 2</Link>
-        </li>
-        <li>
-          <Link href={"/products/3"}>Product 3</Link>
-        </li>
+        {products.map((p) => (
+          <li>
+            <Link href={`/products/${p.id}`}>{p.name}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://6300a18859a8760a757d441c.mockapi.io/products"
+  );
+  const data = await res.json();
+  return { props: { products: data.products } };
 }
