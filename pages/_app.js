b0ca7a09cfe7a19136934a4a6347aa541b9f2f40
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-03-08 15:32:14 +0330

    Shared and per component Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Header from "@/components/Header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,7 @@
 export default function About() {
   return <h1>About</h1>;
 }
+
+About.getLayout = function PageLayout(page) {
+  return <>{page}</>;
+};
