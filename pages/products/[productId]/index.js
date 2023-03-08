076c755c1775cab5074e4680234700438ee4800b
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-03-08 11:37:11 +0330

    add getStaticProps getStaticPaths to productDetail

--- pages/products/[productId]/index.js
+++ pages/products/[productId]/index.js
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router";
 
-export default function ProductDetail() {
+export default function ProductDetail({ product }) {
   const router = useRouter();
   const productId = router.query.productId;
   return (
     <>
       <h1>Product Details</h1>
       {productId}
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
       <button
         onClick={() => {
           router.push("/");
@@ -16,4 +18,27 @@ export default function ProductDetail() {
       </button>
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(
+    "https://6300a18859a8760a757d441c.mockapi.io/products"
+  );
+  const data = await res.json();
+
+  const paths = data.products.map((product) => {
+    return { params: { productId: product.id } };
+  });
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  console.log(context);
+  const res = await fetch(
+    "https://6300a18859a8760a757d441c.mockapi.io/products/" + params.productId
+  );
+  const data = await res.json();
+  return { props: { product: data } };
 }
