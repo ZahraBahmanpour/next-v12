08cba5c56636c33b4adbac61c31d7b69f766da02
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-03-08 14:43:29 +0330

    client-side data fetching and ssr

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,31 @@
 import ProductCard from "@/components/ProductCard";
+import { useEffect, useState } from "react";
 
 export default function Products({ products }) {
-  console.log(products);
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  const [filterText, setFilterText] = useState("");
+  useEffect(() => {
+    const getFilteredProducts = async () => {
+      const res = await fetch(
+        "https://6300a18859a8760a757d441c.mockapi.io/products?name=" +
+          filterText
+      );
+      const data = await res.json();
+      setFilteredProducts(data.products);
+    };
+    getFilteredProducts();
+  }, [filterText]);
   return (
     <>
       <h1>Products</h1>
+      <input
+        type={"text"}
+        placeholder="enter name..."
+        onChange={(e) => setFilterText(e.target.value)}
+        value={filterText}
+      />
       <ul>
-        {products.map((p) => (
+        {filteredProducts.map((p) => (
           <ProductCard {...p} key={p.id} />
         ))}
       </ul>
